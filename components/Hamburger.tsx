a14f344bd349e8ab2372546942bc63a9963a714e
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2022-03-03 20:31:55 +1000

    Finished Navbar

---
+++ components/Hamburger.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Hamburger.module.css'
+
+const Hamburger = () => {
+	return (
+		<div className={styles.hamburger}>
+			<div className={styles.burger}/>
+			<div className={styles.burger}/>
+			<div className={styles.burger}/>
+		</div>
+
+	)
+
+
+}
+
+export default Hamburger
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,29 @@
+import {useState} from 'react'
 import styles from '../styles/Navbar.module.css'
+import Hamburger from './Hamburger'
+
 
 const Navbar = () => {
+
+	const [hamburgerOpen, setHamburgerOpen] = useState(false);
+	const toggleHamburger = () => {
+		setHamburgerOpen(!hamburgerOpen)
+	}
 	return (
-		<nav className={styles.nav}>
-			<ul className={styles.list}>
-				<li className={styles.header}>FantasyAgg</li>
-				<li className={styles.items}>Home</li>
-				<li className={styles.items}>NFL</li>	
-				<li className={styles.items}>MLB</li>
-				<li className={styles.items}>NBA</li>
-			</ul>
-		</nav>
+		<div className={styles.div}>
+			<div className={styles.nav}>
+				<h1 className={styles.header}>FantasyAgg</h1>
+				<ul className={styles.list}>
+					<li className={styles.items}>Home</li>
+					<li className={styles.items}>NFL</li>
+					<li className={styles.items}>MLB</li>
+					<li className={styles.items}>NBA</li>
+				</ul>
+				<div className={styles.hamburger} onClick={toggleHamburger}>
+					<Hamburger/>
+				</div>
+			</div>
+		</div>
 	)
 }
 
